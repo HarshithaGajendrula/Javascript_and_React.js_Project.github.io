83e1b913fd50a2bdec290c1a8e0a84d0bb75ac42
HarshithaGajendrula <71062269+HarshithaGajendrula@users.noreply.github.com>
2021-06-06 11:54:18 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        I'am Harshitha I learnt everything from Scratch including Javascipt
+        React.js, HTML from Shaurya Sir it was amazing to learn.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
